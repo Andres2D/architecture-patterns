2a92a1ecbba584aae5bd695060d55da381268d01
Andres2D <andres2d1997.1206@gmail.com>
2022-09-07 19:20:05 -0500

    Defining the object structure

--- apps/meso/src/app/home/home.component.ts
+++ apps/meso/src/app/home/home.component.ts
@@ -1,6 +1,32 @@
 import { Component, OnInit } from '@angular/core';
 
-const tango = [];
+interface BaseEntity {
+  id: string | null;
+};
+
+interface Client extends BaseEntity {
+  firstName: string;
+  lastName: string;
+  company: string;
+};
+
+const peter: Client = {
+  id: '1',
+  firstName: 'Peter',
+  lastName: 'Porker',
+  company: 'Acme, Inc'
+};
+
+const john: Client = {
+  id: '2',
+  firstName: 'John',
+  lastName: 'Doe',
+  company: 'N/A'
+};
+
+const clients: Client[] = [peter, john];
+
+const tango = clients;
 
 @Component({
   selector: 'fem-home',
